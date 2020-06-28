fc9de520e30522f2c8be96ed24d6a6fd52d810fa
PadmaGnanapriya <padmaisuru@gmail.com>
2020-06-28 12:17:13 +0530

    name the number buttons

--- Calculator/src/MainFormController.java
+++ Calculator/src/MainFormController.java
@@ -1,2 +1,44 @@
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class MainFormController {
+    public JFXTextField txt;
+
+
+
+    private void setText(String value){
+        txt.setText(txt.getText()+value);
+    }
+
+    public void btn1OnAction(ActionEvent actionEvent) {setText("1");
+    }
+
+    public void btn2OnAction(ActionEvent actionEvent) {setText("2");
+    }
+
+    public void btn3OnAction(ActionEvent actionEvent) {setText("3");
+    }
+
+    public void btn4OnAction(ActionEvent actionEvent) {setText("4");
+    }
+
+    public void btn5OnAction(ActionEvent actionEvent) {setText("5");
+    }
+
+    public void btn6OnAction(ActionEvent actionEvent) {setText("6");
+    }
+
+    public void btn7OnAction(ActionEvent actionEvent) {setText("7");
+    }
+
+    public void btn8OnAction(ActionEvent actionEvent) {setText("8");
+    }
+
+    public void btn9OnAction(ActionEvent actionEvent) {setText("9");
+    }
+
+    public void btn0OnAction(ActionEvent actionEvent) {setText("0");
+    }
+
+
 }
